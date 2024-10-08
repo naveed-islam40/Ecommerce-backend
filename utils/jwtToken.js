caf0278192b79b089cdd30d05a6963df22f35408
naveed-islam40 <naveedislam20ni@gmail.com>
2024-10-08 02:12:52 -0700

    changes in Cookies

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,19 +1,18 @@
 const sendToken = (user, statusCode, res) => {
-    const token = user.getJWTToken();
+  const token = user.getJWTToken();
 
-    const options = {
-         maxAge: 24 * 60 * 60 * 1000,
-         httpOnly: true,
-         secure: process.env.NODE_ENV === 'production',
-         sameSite: 'strict'
-    }
+  const options = {
+    maxAge: 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Strict",
+  };
 
-    
-    res.status(statusCode).cookie('token', token, options).json({
-        success: true,
-        token,
-        user
-    });
-}
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+    user,
+  });
+};
 
-module.exports = sendToken
+module.exports = sendToken;
